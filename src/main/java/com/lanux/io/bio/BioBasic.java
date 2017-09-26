3a33928c3f8b230a21fc61bb8122104aa75913e3
zhongzhenlong <zhongzhenlong@modernavenue.com>
2017-09-26 17:35:43 +0800

    byte

--- src/main/java/com/lanux/io/bio/BioBasic.java
+++ src/main/java/com/lanux/io/bio/BioBasic.java
@@ -11,8 +11,7 @@ import java.io.OutputStream;
  * Created by lanux on 2017/9/18.
  */
 public class BioBasic {
-    private static final int LENGTH_VALUE_BYTES = 4;
-
+    private static final int LENGTH_VALUE_BYTES = 4;// 报文内容长度值
     private static final int BYTE_BUFFER = 1024;
 
     public byte[] readStream2(InputStream inputStream) throws IOException {
@@ -24,17 +23,13 @@ public class BioBasic {
         while ((readLen = inputStream.read(bytes)) != -1) {
             hadRead += readLen;
             if (bodyLength == -1) {
-                if (readLen < LENGTH_VALUE_BYTES) {
-                    continue;
-                }
+                if (hadRead < LENGTH_VALUE_BYTES) continue;
                 bodyLength = ByteUtil.byteArrayToInt(bytes);
-                bytes = new byte[BYTE_BUFFER];
-                hadRead = 0;
+                bytes = new byte[BYTE_BUFFER];// 开始读取报文内容,字节数组用长一点的
+                hadRead = 0;// 开始读取报文内容，hadRead重置0
             } else {
                 outSteam.write(bytes, 0, readLen);
-                if (hadRead == bodyLength) {
-                    break;
-                }
+                if (hadRead == bodyLength) break;
             }
         }
         outSteam.flush();
@@ -52,18 +47,14 @@ public class BioBasic {
             limit -= readLen;
             if (bodyLength == -1) {
                 // 报文内容长度值还未读取出来
-                if (offset < LENGTH_VALUE_BYTES) {
-                    continue;
-                }
+                if (offset < LENGTH_VALUE_BYTES)continue;
                 bodyLength = ByteUtil.byteArrayToInt(bytes);
-                bytes = new byte[bodyLength];
-                limit = bodyLength;
-                offset = 0;
+                bytes = new byte[bodyLength];// 开始读取报文内容,初始化报文长度相当的字节数组来承载报文内容
+                limit = bodyLength;// 初始化 报文内容剩余待读长度= bodyLength
+                offset = 0;// 开始读取报文内容，offset重置0
             } else {
-                // 报文内容长度已经读取
                 if (offset == bodyLength) {
-                    // 报文内容已经读取完毕
-                    break;
+                    break;// 报文内容已经读取完毕
                 }
             }
         }
