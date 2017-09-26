420224ec2d8004963a53eaf75f5526a63a08c337
zhongzhenlong <zhongzhenlong@modernavenue.com>
2017-09-26 17:18:56 +0800

    byte

--- src/main/java/com/lanux/io/bio/BioBasic.java
+++ src/main/java/com/lanux/io/bio/BioBasic.java
@@ -10,27 +10,30 @@ import java.io.OutputStream;
  * Created by lanux on 2017/9/18.
  */
 public class BioBasic {
-    private static final int LENGTH_BYTES = 4;
+    private static final int LENGTH_VALUE_BYTES = 4; // 报文内容长度值 占用字节数
 
     public byte[] readStream(InputStream inputStream) throws IOException {
-        byte[] bytes = new byte[LENGTH_BYTES];
+        byte[] bytes = new byte[LENGTH_VALUE_BYTES];
         int readLen;//本次读取的字节数
-        int position = 0;//已经读取数据的下一个位置
         int bodyLength = -1;// 报文内容长度
-        int limit = LENGTH_BYTES;
-        while ((readLen = inputStream.read(bytes, position, limit)) != -1) {
-            position += readLen;
+        int offset = 0;//已经读取数据的偏移量
+        int limit = LENGTH_VALUE_BYTES;// input stream 读取长度逐渐减少
+        while ((readLen = inputStream.read(bytes, offset, limit)) != -1) {
+            offset += readLen;
             limit -= readLen;
             if (bodyLength == -1) {
-                if (position < LENGTH_BYTES) {
+                // 报文内容长度值还未读取出来
+                if (offset < LENGTH_VALUE_BYTES) {
                     continue;
                 }
                 bodyLength = ByteUtil.byteArrayToInt(bytes);
                 bytes = new byte[bodyLength];
                 limit = bodyLength;
-                position = 0;
+                offset = 0;
             } else {
-                if (position == bodyLength) {
+                // 报文内容长度已经读取
+                if (offset == bodyLength) {
+                    // 报文内容已经读取完毕
                     break;
                 }
             }
